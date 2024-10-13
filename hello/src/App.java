628dbe7d401050ac030f58195be1fa77cb7f8e48
Pragyahanvat7 <103772734+Pragyahanvat7@users.noreply.github.com>
2024-10-14 00:35:03 +0530

    Add files via upload

---
+++ hello/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    static int solve(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int arr[] = {1,2,3,4,5};
+        Array
+        for(int i=0; i<arr.length-1; i++){
+
+        }
+
+    }
+}
