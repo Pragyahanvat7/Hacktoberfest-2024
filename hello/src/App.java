1bd7cc1654fb32536c708b09370c5acbf626fc59
nick gaur <67472332+nickgaur@users.noreply.github.com>
2024-10-14 23:46:04 +0530

    increased the number

--- hello/src/App.java
+++ hello/src/App.java
@@ -4,7 +4,7 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        int arr[] = {1,2,3,4,5,6};
+        int arr[] = {1,2,3,4,5,6,7};
         Array
         for(int i=0; i<arr.length-1; i++){
 
