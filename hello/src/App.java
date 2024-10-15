f362d20ac0b0ad4de118e8d23a7cc286d66c1c50
nick gaur <67472332+nickgaur@users.noreply.github.com>
2024-10-16 05:21:53 +0530

    implemented the function for adding two integers

--- hello/src/App.java
+++ hello/src/App.java
@@ -1,13 +1,12 @@
 public class App {
     static int solve(int a, int b) {
-        return 0;
+        return a+b;
     }
 
     public static void main(String[] args) throws Exception {
         int arr[] = {1,2,3,4,5,6};
-        Array
         for(int i=0; i<arr.length-1; i++){
-
+            System.out.println(solve(arr[i], arr[i+1]));   
         }
 
     }
